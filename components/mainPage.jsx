35a720494c9c6b4ab03c6ef1c0be001313d43ced
myfishnameisqwerty <v_jenya@yahoo.com>
2021-02-01 00:01:14 +0200

    main added

--- components/mainPage.jsx
+++ components/mainPage.jsx
@@ -4,31 +4,62 @@ class MainPage extends Component {
     render() { 
         return ( 
             <main>
-                <div className='container'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nisi praesentium similique suscipit necessitatibus totam, ducimus, est, exercitationem repellat eum nam aliquam provident excepturi soluta minima fuga rerum magnam! Aspernatur!
-                    Mollitia saepe natus minima aliquid delectus expedita voluptatum porro id neque libero nobis ducimus labore quidem enim adipisci voluptas eius, iusto, tempora magnam odit recusandae. Unde obcaecati aliquam nesciunt molestias!
-                    Aperiam, eum repellendus laudantium molestias provident aspernatur distinctio ab. Excepturi labore, eaque exercitationem ullam natus aspernatur eos explicabo inventore rerum, vel nam ex? Accusamus a placeat cumque repudiandae expedita distinctio.
-                    Deserunt aperiam esse atque exercitationem omnis nisi optio voluptatibus quas modi assumenda, nobis enim facilis officiis. Aliquid odio perspiciatis esse! Itaque amet unde rem quaerat sunt! Earum at temporibus libero?
-                    Magnam tenetur quae rerum, illo qui perspiciatis! Nisi necessitatibus consectetur voluptates quam nihil natus! Quidem ducimus perspiciatis sint reiciendis in sapiente est at, sunt porro eius tempora officiis harum ad.
-                    Ratione quasi accusamus magni voluptates quia deleniti, ipsum impedit dolore sequi, alias cupiditate, ex quam tenetur. Libero, qui. Veritatis unde dolorem laborum alias commodi qui illum eum excepturi aperiam eius.
-                    Nam neque et ad numquam recusandae, cum voluptatibus laudantium expedita quod rerum blanditiis nobis, enim animi provident eaque optio dicta similique? Facilis explicabo odit nemo sint dolorem quae totam et?
-                    A porro ipsa accusantium, vitae in minus voluptate voluptatem accusamus nisi saepe dignissimos, sit ipsam explicabo delectus? Veritatis beatae ut porro impedit facere blanditiis asperiores repellendus? Dicta totam quidem dolorem?
-                    Atque inventore, sed earum fugiat quibusdam alias illo nulla optio quis ullam debitis non temporibus voluptates voluptatibus. Quam in, quaerat voluptate perspiciatis odit ipsa esse quas ab similique magnam possimus?
-                    Eaque repellendus aliquid dolorem consequuntur. Aliquid minus corporis autem unde dolorem tempora, laboriosam ad nihil eos pariatur consequuntur nobis facere nisi vero ipsa neque vel nostrum nulla sapiente sunt perferendis?
-                    Blanditiis, nesciunt. Dolorum, et delectus! Aliquid ex molestiae cumque blanditiis illo nulla reiciendis iusto optio architecto animi amet provident a deleniti dolores mollitia dolorem quaerat rem, eius nobis unde dicta.
-                    Doloremque qui odit neque maxime perferendis, provident officiis voluptatem quae, quia reprehenderit ducimus dicta soluta ullam eos praesentium. Nemo provident voluptatibus exercitationem sequi? Nostrum inventore, autem aliquid maiores temporibus enim?
-                    Veritatis vel aliquid culpa perferendis, et laudantium, molestiae cupiditate excepturi repellat quam ab. Nostrum, tenetur, modi ullam vel rem dolorem mollitia corrupti quo recusandae dignissimos, ratione eaque? Beatae, nisi facilis!
-                    Possimus nam accusantium omnis dolore consequatur dicta aliquid odio, repellendus veritatis ex. Esse ratione dolorum deserunt tempore quasi! Facilis obcaecati quam mollitia ab quisquam hic maiores quas. Quibusdam, debitis impedit?
-                    Fugiat blanditiis libero consequuntur dolor beatae, ipsam omnis repellendus quod doloremque mollitia sequi, quos id tenetur, odio hic. Voluptatibus facere accusamus saepe eligendi, fugiat laboriosam explicabo. Fugit eum itaque dolores.
-                    Vero nemo eum quaerat dolores laborum consequatur? Nam autem, aliquid officia ipsum omnis, praesentium, voluptatibus expedita ipsa molestias qui velit adipisci necessitatibus nesciunt? Dolorem magni tempora asperiores consectetur saepe. Quos!
-                    Odio nesciunt ullam, ipsa esse porro sapiente a iure similique vel dolorem dolorum nostrum? Fuga provident quos aut dolore sequi officia alias, quibusdam sapiente ipsum, voluptas debitis suscipit quas. Dolores.
-                    Enim facere quis praesentium exercitationem saepe doloribus sint voluptates. Ut nihil recusandae culpa consequuntur veniam corrupti voluptatibus iure, perspiciatis temporibus quibusdam assumenda voluptas modi blanditiis quisquam vitae fugit similique non!
-                    Itaque autem totam quod? Debitis enim aliquam adipisci qui vero deleniti ratione a eius autem nisi eligendi nihil, deserunt earum inventore sed, voluptatem sapiente ipsum blanditiis facere rem delectus? Repudiandae.
-                    Ut sequi quae nulla nemo molestias pariatur, optio quis praesentium iusto perspiciatis asperiores iste inventore atque assumenda fugiat itaque quas nostrum, dolorum fugit molestiae alias laboriosam voluptas eveniet recusandae. Aperiam!
+                <div className='container bg-light pb-5'>
+                    <br/><br/><br/>
+                    <div className='stars' style ={starLike}>
+                    <i className="fas fa-star" ></i>
+                    <i className="fas fa-star" ></i>
+                    <i className="fas fa-star" ></i>
+                    <i className="fas fa-star" ></i>
+                    <i className="far fa-star" ></i>
+                    </div>
+                    <div className='d-md-flex'>
+                    <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+                        <div className="carousel-inner"
+                        style={imageStyle}>
+                            
+                            <div className="carousel-item active">
+                                <img className="d-block w-100" src={imageList[0]} alt="First slide"
+                                />
+                            </div>
+                            <div className="carousel-item">
+                                <img className="d-block w-100" src={imageList[1]} alt="Second slide"
+                                />
+                            </div>
+                            <div className="carousel-item">
+                                <img className="d-block w-100" src={imageList[2]} alt="Third slide"
+                                />
+                            </div>
+                        </div>
+                        <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="sr-only">Previous</span>
+                        </a>
+                        <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="sr-only">Next</span>
+                        </a>
+                        </div>
+                        <div className='ml-4'>
+                            <p id='foodName'>Chebureki x10</p>
+                            <p id='foodDesc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nisi, harum incidunt placeat assumenda dignissimos vitae obcaecati dolorem officiis expedita, cum cumque facere veniam impedit dolores saepe nulla sed magni?</p>
+                        </div> 
+                    </div>
+                    
+                    <div className='price' style ={starLike}><b>120₪ </b> 
+                    <i className="fas fa-plus-circle"></i></div>
                 </div>
             </main>
          );
     }
 }
- 
+const imageStyle = {
+    width: 300
+}
+const starLike = {
+    color: 'red'
+}
+
+const imageList = ["https://cdn.lifehacker.ru/wp-content/uploads/2020/01/Kak-prigotovit-idealnye-domashnie-chebureki_1579377232.jpg", "https://www.povarenok.ru/data/cache/2017oct/23/16/2156089_32368-710x550x.jpg", "https://img-global.cpcdn.com/recipes/ee9394e8a939297c/1200x630cq70/photo.jpg"];
+
 export default MainPage;
